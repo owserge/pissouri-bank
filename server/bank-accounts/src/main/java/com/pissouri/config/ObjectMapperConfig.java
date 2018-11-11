0e826a7451ca28bfc905fd030090a81b2ab45aef
George Christou <christgf@gmail.com>
2018-11-12 00:49:46 +0200

    Spring configuration take 2

--- server/bank-accounts/src/main/java/com/pissouri/config/ObjectMapperConfig.java
+++ server/bank-accounts/src/main/java/com/pissouri/config/ObjectMapperConfig.java
@@ -1,9 +1,7 @@
 package com.pissouri.config;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,17 +9,14 @@ import org.springframework.context.annotation.Configuration;
 import static com.fasterxml.jackson.databind.PropertyNamingStrategy.SNAKE_CASE;
 
 @Configuration
-public class JacksonConfig {
+public class ObjectMapperConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
 
         return new ObjectMapper()
-                .registerModule(new Jdk8Module())
                 .registerModule(new JavaTimeModule())
                 .setPropertyNamingStrategy(SNAKE_CASE)
-                .configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE, false)
-                .configure(SerializationFeature.WRITE_DATES_WITH_ZONE_ID, false)
                 .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
     }
 }
